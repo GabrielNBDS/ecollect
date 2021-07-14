4a936a89e7b84fc019fe8724bed9381cb87de164
GabrielNBDS <gabriel.nbds@gmail.com>
2021-07-14 16:19:27 -0300

    fix: Updated Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,69 +1,28 @@
 import React from 'react';
-import {
-  Icon,
-  Button,
-  Image,
-  Container,
-  Flex,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverTrigger,
-  LightMode,
-} from '@chakra-ui/react';
+import { Icon, Image, Container, Flex, IconButton } from '@chakra-ui/react';
 import { FiPower } from 'react-icons/fi';
 
 import { useAuth } from '../hooks/auth';
 
 const Header: React.FC = () => {
-  const { user, signOut } = useAuth();
+  const { signOut } = useAuth();
 
   return (
-    <Flex as="header" bg="green.100" w="100vw" height="70px">
+    <Flex as="header" width="100%" height="70px">
       <Container
         display="flex"
         alignItems="center"
         justifyContent="space-between"
         maxW="1200px"
       >
-        <Image w="160px" src="/assets/logo.svg" />
+        <Image h={['40px']} src="/assets/logo.svg" />
 
-        <Popover>
-          <PopoverTrigger>
-            <Button
-              height="60px"
-              width="60px"
-              _focus={{ boxShadow: 'none' }}
-              variant="unstyled"
-            >
-              <Image
-                src={user.photoURL}
-                borderRadius="full"
-                width="60px"
-                border="2px solid white"
-              />
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent>
-            <PopoverArrow />
-            <PopoverCloseButton />
-            <PopoverBody>
-              <LightMode>
-                <Button
-                  color="white"
-                  size="sm"
-                  colorScheme="red"
-                  rightIcon={<Icon as={FiPower} />}
-                  onClick={signOut}
-                >
-                  Logout
-                </Button>
-              </LightMode>
-            </PopoverBody>
-          </PopoverContent>
-        </Popover>
+        <IconButton
+          onClick={signOut}
+          aria-label="logout"
+          colorScheme="red"
+          icon={<Icon as={FiPower} />}
+        />
       </Container>
     </Flex>
   );
