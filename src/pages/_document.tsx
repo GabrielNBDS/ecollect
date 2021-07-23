6b67e2411af9dbf941a276602f411aa36eb2c741
GabrielNBDS <gabriel.nbds@gmail.com>
2021-07-23 18:20:27 -0300

    feat: Added favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,6 +16,27 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#ff0000" />
+          <meta name="theme-color" content="#ffffff" />
           <link rel="stylesheet" type="text/css" href="/nprogress.css" />
         </Head>
         <body>
